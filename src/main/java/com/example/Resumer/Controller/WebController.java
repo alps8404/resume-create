2f72b77bce5d3dd3d80dd3427517977433504d01
alps8404 <alpeshgharaniya8404@gmail.com>
2025-06-07 12:07:15 +0530

    Added webConfig

---
+++ src/main/java/com/example/Resumer/Controller/WebController.java
@@ -0,0 +1,15 @@
+package com.example.Resumer.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping(value = "/{[path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/index.html";
+    }
+}
+
+
